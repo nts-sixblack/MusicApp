4f8c13d4ee34e45ef90228af7c6ad88b4bfc7c12
sixblack <nts.sixblack@gmail.com>
2022-08-23 20:53:22 +0700

    swagger

--- src/main/java/app/music/musicapp/MusicAPpApplication.java
+++ src/main/java/app/music/musicapp/MusicAPpApplication.java
@@ -1,9 +1,11 @@
 package app.music.musicapp;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition
 public class MusicAPpApplication {
 
     public static void main(String[] args) {
